60e3c1dc4d44b2a441c4debb8d375ceea3648a44
ssuvorov <s.suvoroff@gmail.com>
2022-09-17 20:13:18 +0000

    Deploying to gh-pages from @ The-Pirate-Cave/pirate-map@f7435f35ada79c4ec73edce160886c54dc6f792c 🚀

---
+++ static/SUUt9WOrWEghbrBdnjJAE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2c796e83-d0894c4090c04439.js","static/chunks/257-79a17327ad793ccd.js","static/chunks/pages/index-757ca1475c4e10b6.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
