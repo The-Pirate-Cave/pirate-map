abfa4d9191be54880026b4784fe5a348fdef4b5f
example <sergey@daoism.systems>
2022-09-17 23:15:32 +0200

    font fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ const Home: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300,500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" />
