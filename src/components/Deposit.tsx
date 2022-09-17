0d21c723581114c09999aeb25c3c34c8e3f53605
D3Portillo <d3portillo@gmail.com>
2022-09-17 14:45:01 +0200

    chores: add void hanlder to #Deposit

--- src/components/Deposit.tsx
+++ src/components/Deposit.tsx
@@ -1,6 +1,23 @@
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { noOp } from 'src/lib/helpers';
+import { useAccount } from 'wagmi';
+
 function Deposit() {
+  const { address } = useAccount();
+  const { openConnectModal = noOp } = useConnectModal();
+  function handleDeposit() {
+    if (!address) {
+      openConnectModal();
+    } else {
+      // noop
+    }
+  }
+
   return (
-    <button className="w-[150px] rounded-xl border bg-indigo-600 p-2 px-4 text-white">
+    <button
+      onClick={handleDeposit}
+      className="w-[150px] rounded-xl border bg-indigo-600 p-2 px-4 text-white"
+    >
       Deposit
     </button>
   );
